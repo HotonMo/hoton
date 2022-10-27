e5cc730ba461bf14cc10d364b5970580e0b65177
hoton <i.hoton19@gmail.com>
2022-10-27 09:54:48 +0300

    Initial Commit

---
+++ hoton/hotonApp.swift
@@ -0,0 +1,17 @@
+//
+//  hotonApp.swift
+//  hoton
+//
+//  Created by hoton on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct hotonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
